b7818325ee113f1bcef350564f1a92b273eb398f
TonyXY <TonyXY@users.noreply.github.com>
2026-08-01 16:24:24 +0000

    deploy: 2810ab12667831f6a58e501714e1ad282f69cee2

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.DWiUgFRx.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.DS8lVELQ.js";import{_ as a}from"../app.CEvEvLMn.js";import"./chunk-5VM5RSS4.DcqwPMQn.js";import"./chunk-XXDRQBXY.4R0kXtJC.js";import"./chunk-VR4S4FIN.SO0eRmoC.js";import"./chunk-32BRIVSS.CXXU6e4k.js";import"./channel.Bf7kecEM.js";import"./framework.NjEjlYNI.js";import"./theme.BZu-bQS4.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
